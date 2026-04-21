5c1eea7057c9be7d2b7c55522a78ff376efc4b15
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-04-21 12:48:04 +0530

    user update

--- api/user/models.py
+++ api/user/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email=email, password=password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    name           = models.CharField(max_length=50, default='Anonymous')
+    email          = models.EmailField(max_length=254, unique=True)
+    phone          = models.CharField(max_length=20, blank=True, null=True)
+    gender         = models.CharField(max_length=10, blank=True, null=True)
+    session_token  = models.CharField(max_length=10, default=0)
+
+    is_active      = models.BooleanField(default=True)
+    is_staff       = models.BooleanField(default=False)
+
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD  = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+
+# class CustomUser(AbstractUser):
+#     name = models.CharField(max_length=50, default='Anonymous')
+#     email = models.EmailField(max_length=254, unique=True )
+
+#     username=models.CharField(max_length=150, blank=True)
+
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+
+#     phone = models.CharField(max_length=20, blank=True, null=True)
+#     gender = models.CharField(max_length=10, blank=True, null=True)
+
+#     session_token = models.CharField(max_length=10, default=0)
+
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
