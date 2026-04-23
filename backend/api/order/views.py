f644039ecd0161f37f7b044ca549108481b5475c
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-04-23 10:06:00 +0530

    wrap in backend folder

--- backend/api/order/views.py
+++ backend/api/order/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from django.http import JsonResponse 
 from django.contrib.auth import get_user_model
-from. serializers import OrderSerializer
-from . models import Order
+from.serializers import OrderSerializer
+from .models import Order
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
