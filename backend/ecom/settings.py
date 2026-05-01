bd9365c7fc6c39e27e165331e5f2995e949452df
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-05-01 13:40:53 +0530

    update vercel deployment

--- backend/ecom/settings.py
+++ backend/ecom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-eeslere11ui=gv-y19+5$i1-wo^7m=3wq2p#xpsda-4)a-npm@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
