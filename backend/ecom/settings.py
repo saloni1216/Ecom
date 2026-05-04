2981f43ad9a5e05439274adfd392652fdd6d9695
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-05-04 15:03:31 +0530

    .......

--- backend/ecom/settings.py
+++ backend/ecom/settings.py
@@ -77,13 +77,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
 
-# ✅ UPDATED DATABASE CONFIG (Vercel friendly)
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
