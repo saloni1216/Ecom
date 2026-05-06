85ba28091707f8210ba1ea354a5fcdfc2caab2e1
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-05-06 13:11:08 +0530

    setting

--- backend/ecom/settings.py
+++ backend/ecom/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
 
-    
+     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
 
@@ -140,7 +140,7 @@ CLOUDINARY_STORAGE = {
 }
 STORAGES = {
     "default": {
-        "BACKEND": "cloudinary.storage.MediaCloudinaryStorage",  # cloudinary_storage → cloudinary
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
