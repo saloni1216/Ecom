d085d9e2f90b1d99ea785c67503eb1843dd7022e
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-05-06 12:03:33 +0530

    ENV

--- backend/ecom/settings.py
+++ backend/ecom/settings.py
@@ -33,6 +33,8 @@ INSTALLED_APPS = [
     "api.user",
     "api.order",
     "api.payment",
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,11 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ]
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
