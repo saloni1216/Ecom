4c049999025756a1dcf4afd2281eb76aa4641d7d
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-05-06 15:00:03 +0530

    product model  update

--- backend/api/product/models.py
+++ backend/api/product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from api.category.models import Category
+from cloudinary.models import CloudinaryField
 
 
 
@@ -9,7 +10,8 @@ class Product(models.Model):
     price = models.CharField(max_length=50)
     stock = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True, blank=True)
-    image = models.ImageField(upload_to="images", blank=True, null=True)
+    # image = models.ImageField(upload_to="images", blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_to = models.DateTimeField(auto_now=True)
